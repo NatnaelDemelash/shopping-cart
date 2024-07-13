873fb417ac9eab0d8f76b2a1f3cdc09096a1e418
Natnael Demelash <natidemelash074@gmail.com>
2024-07-13 21:04:28 +0300

    add and remove button for products

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,11 +1,4 @@
-import {
-  Minus,
-  MinusCircleIcon,
-  MinusSquareIcon,
-  Plus,
-  PlusCircleIcon,
-  PlusSquareIcon,
-} from 'lucide-react';
+import { MinusSquareIcon, PlusSquareIcon } from 'lucide-react';
 import { Button } from './ui/button';
 import {
   Card,
@@ -15,6 +8,7 @@ import {
   CardHeader,
   CardTitle,
 } from './ui/card';
+import { formatCurrency } from '../utils/formatCurrency';
 
 type StoreItemProps = {
   id: number;
@@ -24,11 +18,12 @@ type StoreItemProps = {
 };
 
 const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+  const quantity = 0;
   return (
     <Card key={id} className="rounded-xl px-2 m-4">
       <CardHeader>
         <CardTitle>{name}</CardTitle>
-        <CardDescription>${price}</CardDescription>
+        <CardDescription>{formatCurrency(price)}</CardDescription>
       </CardHeader>
       <CardContent>
         <img
@@ -40,14 +35,33 @@ const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
       </CardContent>
       <hr className="h-[.02rem] bg-blue-200 border-none mb-6" />
       <CardFooter>
-        <div className="flex gap-4 items-center">
-          <Button variant="ghost">
-            <PlusSquareIcon height={40} width={40} />
+        {quantity === 0 ? (
+          <Button variant="outline" className="border-slate-900 rounded-lg">
+            + Add To Cart
           </Button>
-          <Button variant="ghost" size="icon">
-            <MinusSquareIcon height={40} width={40} />
-          </Button>
-        </div>
+        ) : (
+          <div className="flex flex-col justify-center gap-2 items-center">
+            <div className="flex justify-center items-center">
+              <Button variant="ghost">
+                <PlusSquareIcon height={35} width={35} />
+              </Button>
+              <div>
+                <span className="text-2xl font-semibold">{quantity}</span> in
+                cart
+              </div>
+              <Button variant="ghost" size="icon" className="ml-3">
+                <MinusSquareIcon height={35} width={35} />
+              </Button>
+            </div>
+
+            <Button
+              variant="destructive"
+              className="bg-red-400 rounded-xl mt-4 text-white"
+            >
+              Clear from Cart
+            </Button>
+          </div>
+        )}
       </CardFooter>
     </Card>
   );
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -4,7 +4,7 @@ import storeItems from '../data/items.json';
 const Store = () => {
   return (
     <>
-      <h1>Store</h1>
+      <h1 className="text-3xl font-extralight ml-6">Recently Added Gadgets</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {storeItems.map((item, index) => (
           <StoreItem key={index} {...item} />
