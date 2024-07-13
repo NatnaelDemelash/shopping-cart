156dd6f95fdc542a7c68de9d2c47b82471ae35e1
Natnael Demelash <natidemelash074@gmail.com>
2024-07-13 10:29:33 +0300

    created a navbar using shadcn menubar

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Store from './pages/Store';
 import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div className="container mx-auto m-4 px-2 md:m-8 md:px-6">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <>
+      <Navbar />
+      <div className="container mx-auto m-4 px-2 md:m-8 md:px-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { Menubar, MenubarMenu, MenubarTrigger } from './ui/menubar';
+import { ShoppingCartIcon } from 'lucide-react';
+
+const Navbar = () => {
+  return (
+    <nav className="sticky shadow-md flex justify-between items-center px-6 py-2 md:px-8 bg-purple-200 h-20">
+      <Menubar>
+        <MenubarMenu>
+          <Link to="/">
+            <MenubarTrigger>Home</MenubarTrigger>
+          </Link>
+          <Link to="/about">
+            <MenubarTrigger>About</MenubarTrigger>
+          </Link>
+          <Link to="/store">
+            <MenubarTrigger>Store</MenubarTrigger>
+          </Link>
+        </MenubarMenu>
+      </Menubar>
+      <button className="relative">
+        <ShoppingCartIcon
+          className="cursor-pointer relative"
+          width={25}
+          height={25}
+        />
+        <div className="rounded-full bg-red-500 text-white absolute -right-4 -bottom-2 w-[1.5rem] h-[1.5rem] transform translate-x-1/4 translate-y-1/4">
+          3
+        </div>
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
